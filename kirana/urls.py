52ca194e1dc7f43a14da1603073c0b0e11106a77
Rahul Developer <rahul@kirana.dev>
2026-03-15 14:26:45 +0530

    Fix: Add missing JsonResponse import in urls.py

--- kirana/urls.py
+++ kirana/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 
